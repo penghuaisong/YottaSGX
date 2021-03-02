6ec91db21ffdd2601fbca0474f95ac229c6565fc
penghuaisong <Peng405560>
2021-03-02 14:17:02 +0800

    update

--- controller/UserController.go
+++ controller/UserController.go
@@ -2,12 +2,8 @@ package controller
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
-	"github.com/yottachain/YTCoreService/api"
-	"github.com/yottachain/YTCoreService/env"
 )
 
 //User 用户注册
@@ -20,7 +16,7 @@ type User struct {
 func Register(g *gin.Context) {
 	// defer env.TracePanic("Register")
 	var json User
-	ii := 1
+	// ii := 1
 
 	if err := g.Bind(&json); err != nil {
 		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -29,33 +25,34 @@ func Register(g *gin.Context) {
 
 	privateKey := json.PrivateKey
 
-	var client *api.Client
-	var err2 error
-	for {
-		// client, err2 = api.NewClient(userName, privateKey)
-		client, err2 = api.NewClientV2(&env.UserInfo{
-			UserName: userName,
-			Privkey:  []string{privateKey}}, 3)
-		if err2 != nil {
-			ii++
-			if ii <= 3 {
-				time.Sleep(time.Second * 5)
-			} else {
-				logrus.Infof("err:%s\n", err2)
-				break
-			}
-		} else {
-			break
-		}
-	}
-	if err2 != nil {
-		logrus.Errorf("User Register Failed, %s\n", err2)
-		g.JSON(http.StatusUnauthorized, gin.H{"status": http.StatusUnauthorized, "Msg": "Register Failed!Please checked userName and privateKey "})
-	} else {
+	// var client *api.Client
+	// var err2 error
+	// for {
+	// 	// client, err2 = api.NewClient(userName, privateKey)
+	// 	client, err2 = api.NewClientV2(&env.UserInfo{
+	// 		UserName: userName,
+	// 		Privkey:  []string{privateKey}}, 3)
+	// 	if err2 != nil {
+	// 		ii++
+	// 		if ii <= 3 {
+	// 			time.Sleep(time.Second * 5)
+	// 		} else {
+	// 			logrus.Infof("err:%s\n", err2)
+	// 			break
+	// 		}
+	// 	} else {
+	// 		break
+	// 	}
+	// }
+	// if err2 != nil {
+	// 	logrus.Errorf("User Register Failed, %s\n", err2)
+	// 	g.JSON(http.StatusUnauthorized, gin.H{"status": http.StatusUnauthorized, "Msg": "Register Failed!Please checked userName and privateKey "})
+	// } else {
 
-		logrus.Infof("PrivateKey::::%s\n", client.AccessorKey)
-		logrus.Infof("User Register Success,UserName: %s\n", userName)
-		g.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "Msg": "Register success " + userName})
-	}
+	// 	logrus.Infof("PrivateKey::::%s\n", client.AccessorKey)
+	// 	logrus.Infof("User Register Success,UserName: %s\n", userName)
+	// 	g.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "Msg": "Register success " + userName})
+	// }
+	g.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "Msg": "Register success " + userName, "privateKey": privateKey})
 
 }
--- go.mod
+++ go.mod
@@ -8,8 +8,6 @@ require (
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/prometheus/common v0.18.0
 	github.com/robfig/cron v1.2.0
-	github.com/sirupsen/logrus v1.6.0
-	github.com/yottachain/YTCoreService v0.0.0-20210126101905-4ba706f73e71
 	golang.org/x/sys v0.0.0-20200819171115-d785dc25833f // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 )
