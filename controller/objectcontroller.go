12ca8b9ba1995454196d81a2c046a818d0bc163c
penghuaisong <306494097@qq.com>
2021-03-04 14:17:09 +0800

    update

--- controller/objectcontroller.go
+++ controller/objectcontroller.go
@@ -97,7 +97,7 @@ func WriteFile(g *gin.Context) {
 	isExist := CheckFileIsExist(directory)
 	fmt.Println("文件是否存在:", isExist)
 
-	if !isExist {
+	if isExist {
 		data := ReadFile(directory)
 
 		fmt.Println("file length:::", len(data))
